ddf6f823f7cdc894454b54717317043f8700d92c
Krish27-Gupta <krishguptazx9@gmail.com>
2026-06-18 14:53:21 +0000

    container config done

---
+++ Javabasic/var.js
@@ -0,0 +1,13 @@
+const accId = 123
+let accCity = "Delhi"
+var accGmail = "K@17"
+accName = 'Krish'
+
+
+console.log(accId)
+
+accCity = "Mumbai"
+accGmail = "KG@18"
+accName = 'Krish Gupta'
+
+console.table([accCity,accGmail,accId,accName])
